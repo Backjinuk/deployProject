9fcf5a5515272dcc9f6da7ea5f963c8b0c90b2d0
BJW <backj28234@gmail.com>
2025-06-05 14:52:28 +0900

    :zap: WebConfig 수정

--- src/main/kotlin/com/deployProject/config/WebConfig.kt
+++ src/main/kotlin/com/deployProject/config/WebConfig.kt
@@ -9,7 +9,7 @@ class WebConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://localhost:8080")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 
     }
