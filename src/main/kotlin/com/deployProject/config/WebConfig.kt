6c18bdd8c65de23c1fef64753e0fa5fb79efb340
BJW <backj123@naver.com>
2026-05-11 11:58:17 +0900

    bjw - cors 정책 수정

--- src/main/kotlin/com/deployProject/config/WebConfig.kt
+++ src/main/kotlin/com/deployProject/config/WebConfig.kt
@@ -10,7 +10,12 @@ class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         // 수정 이유: Origin 값에는 trailing slash가 없어야 매칭이 정확하다.
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://backjin.iptime.org:8080")
+            .allowedOrigins(
+                "http://localhost:3000",
+                "http://localhost:8080",
+                "http://backjin.iptime.org:7070",
+                "http://backjin.iptime.org:8080"
+            )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 
     }
